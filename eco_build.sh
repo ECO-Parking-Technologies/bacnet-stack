61977965ddd36fe0c210b87fba0c5641b4871713
jeff-wittrock <jrwittrock@ecoparkingtech.com>
2021-01-11 13:26:14 -0500

    Adding eco specifi cmake configuration script

---
+++ eco_build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+rm -rf eco_build
+mkdir eco_build
+
+options=" \
+-DBACNET_STACK_BUILD_APPS=OFF \
+-DBACNET_STACK_BUILD_TESTS=OFF \
+-DBAC_ROUTING=OFF \
+-DBACFILE=OFF \
+-DBACNET_PROPERTY_LISTS=ON \
+-DBACNET_BUILD_PIFACE_APP=OFF \
+-DBACDL_ETHERNET=OFF \
+-DBACDL_MSTP=ON \
+-DBACDL_ARCNET=OFF \
+-DBACDL_BIP=ON \
+-DBACDL_BIP6=OFF \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX="../lib"
+"
+
+cd eco_build
+echo building with cmdline "cmake $options .."
+cmake $options ..
+make install
