1011e6d4b672e6217364f8e53141c0a810a6306d
jeff-wittrock <jrwittrock@ecoparkingtech.com>
2021-01-19 10:23:55 -0500

    Setting ECO specific use case options

--- src/bacnet/bacnet-config.h
+++ src/bacnet/bacnet-config.h
@@ -8,6 +8,35 @@
 #define BACNET_VENDOR_ID    0xEC0
 #define BACNET_VENDOR_NAME "ECO Parking Technologies"
 
-
+#define MAX_ACCUMULATORS            0
+#define MAX_ACCESS_CREDENTIALS      0
+#define MAX_ACCESS_DOORS            0
+#define MAX_ACCESS_POINTS           0
+#define MAX_ACCESS_RIGHTSS          0
+#define MAX_ACCESS_USERS            0
+#define MAX_ACCESS_ZONES            0
+#define MAX_ANALOG_INPUTS           0
+#define MAX_ANALOG_OUTPUTS          0
+#define MAX_ANALOG_VALUES           4 // Only one we are using right now
+#define MAX_BINARY_INPUTS           0
+#define MAX_BINARY_OUTPUTS          0
+#define MAX_BINARY_VALUES           0
+#define BACNET_CHANNELS_MAX         0
+#define MAX_COMMAND_ACTIONS         0
+#define MAX_CREDENTIAL_DATA_INPUTS  0
+#define MAX_CHARACTERSTRING_VALUES  0
+#define MAX_INTEGER_VALUES          0
+#define MAX_LOAD_CONTROLS           0
+#define MAX_LIGHTING_OUTPUTS        0
+#define MAX_LIFE_SAFETY_POINTS      0
+#define MAX_MULTISTATE_INPUTS       0
+#define MAX_MULTISTATE_OUTPUTS      0
+#define MAX_MULTISTATE_VALUES       0
+#define MAX_NOTIFICATION_CLASSES    0
+#define BACNET_NETWORK_PORTS_MAX    0
+#define MAX_OCTETSTRING_VALUES      0
+#define MAX_POSITIVEINTEGER_VALUES  0
+#define MAX_SCHEDULES               0
+#define MAX_TREND_LOGS              0
 
 #endif /* _BACNET_CONFIG_H_ */
